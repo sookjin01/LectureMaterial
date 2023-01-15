e3f79976627cd0ebadd300b046c795a52f397e02
sookjin01 <aisz2004@naver.com>
2023-01-15 13:53:08 +0900

    분해할당수업

--- ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-11.분해할당.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-11.분해할당.js
@@ -1,7 +1,39 @@
+"use strict";
 
 /*
  (구조) 분해 할당에 대해서 알아본다.
 
 
 */
-"use strict";
+var points = [20, 30, 40];
+var x1 = points[0];
+var y1 = points[1];
+var z1 = points[2];
+console.log(x1, y1, z1);
+var x2 = points[0],
+  y2 = points[1],
+  z2 = points[2];
+console.log(x2, y2, z2);
+var x3 = points[0],
+  z3 = points[2];
+console.log(x3, z3);
+var x4 = points[0],
+  y4 = points[1];
+console.log(x4, y4);
+var car = {
+  type: 't',
+  color: 's',
+  medel: '2017'
+};
+// 배열은 순서로 분해 할당을 처리한다
+
+var type1 = car.type;
+var color1 = car.color;
+var medel1 = car.medel;
+console.log(type1, color1, medel1);
+var type = car.type,
+  color = car.color,
+  medel = car.medel,
+  gear = car.gear;
+console.log(type, color, medel, gear);
+// 객체는 프로퍼티 명으로 매칭한다.
--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-11.분해할당.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-11.분해할당.js
@@ -1,6 +1,35 @@
-
 /*
  (구조) 분해 할당에 대해서 알아본다.
 
 
 */
+const points = [20, 30, 40];
+const x1 = points[0];
+const y1 = points[1];
+const z1 = points[2];
+console.log(x1, y1, z1);
+
+const [x2, y2, z2] = points;
+console.log(x2, y2, z2);
+
+const [x3, , z3] = points;
+console.log(x3, z3);
+
+const [x4, y4] = points;
+console.log(x4, y4);
+
+const car = {
+  type: 't',
+  color: 's',
+  medel: '2017',
+};
+// 배열은 순서로 분해 할당을 처리한다
+
+const type1 = car.type;
+const color1 = car.color;
+const medel1 = car.medel;
+console.log(type1, color1, medel1);
+
+const { type, color, medel, gear } = car;
+console.log(type, color, medel, gear);
+// 객체는 프로퍼티 명으로 매칭한다.
