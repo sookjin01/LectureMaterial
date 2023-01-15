88048ef09a0c7deaf2120799ab959e2b291b946d
sookjin01 <aisz2004@naver.com>
2023-01-15 13:27:35 +0900

    백틱 실습

--- ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
@@ -1,6 +1,12 @@
+"use strict";
+
 /*
 
     ES2015의 template literal( 백틱, ``) 에 대해서 알아본다.
-    
+
 */
-"use strict";
+
+var string1 = "\uC548\uB155\uD558\uC138\uC694";
+var string2 = "\uBC18\uAC11\uC2B5\uB2C8\uB2E4.";
+var preeting = "".concat(string1, ", ").concat(string2);
+console.log(preeting);
--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
@@ -1,5 +1,10 @@
 /*
 
     ES2015의 template literal( 백틱, ``) 에 대해서 알아본다.
-    
+
 */
+
+const string1 = `안녕하세요`;
+const string2 = `반갑습니다.`;
+const preeting = `${string1}, ${string2}`;
+console.log(preeting);
