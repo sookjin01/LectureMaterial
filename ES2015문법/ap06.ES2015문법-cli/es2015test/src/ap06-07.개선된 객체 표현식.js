aa9a464371b01d20ea6ee254c111f5183ed4aa5b
sookjin01 <aisz2004@naver.com>
2023-01-15 13:22:26 +0900

    오전수업자료

--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-07.개선된 객체 표현식.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-07.개선된 객체 표현식.js
@@ -1,6 +1,22 @@
 /*
 
-    개선된 객체 표현식에 대해서 알아본다. 
+    개선된 객체 표현식에 대해서 알아본다.
       개선된 객체 프러퍼티 표현식
       개선된 객체 메서드 표현식
 */
+
+const name = '홍길동';
+
+const age = 20;
+
+const p2 = {
+  name,
+  age,
+  powerYell() {
+    console.log(`이름이 : ${this.name} 입니다.`);
+  },
+  setAge(h) {
+    this.age = h;
+    console.log(`나이가 : ${this.age} 입니다.`);
+  },
+};
