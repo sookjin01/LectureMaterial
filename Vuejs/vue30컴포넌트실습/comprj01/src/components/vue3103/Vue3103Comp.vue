874e3d7593bba051ec54bd63aca59c122af07923
sookjin01 <aisz2004@naver.com>
2023-02-05 11:03:09 +0900

    prop 외 세팅 연습

---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/components/vue3103/Vue3103Comp.vue
@@ -0,0 +1,30 @@
+<style scoped>
+span {
+  border: 1px dashed blue;
+  background-color: lightgray;
+  padding: 5px;
+}
+</style>
+
+<template>
+  <div>
+    파일 컴포넌트가 등록되었습니다!
+    <span v-on:click="lclick">
+      {{ localmsg }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['localmsg'],
+  data() {
+    return {};
+  },
+  methods: {
+    lclick(e) {
+      console.log(e.target);
+    },
+  },
+};
+</script>
---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/views/Vue3103View.vue
@@ -0,0 +1,25 @@
+<style scoped></style>
+
+<template>
+  <div id="app">
+    <h2>컴포넌트 등록</h2>
+    <Vue3103Comp v-bind:localmsg="localmsg1"></Vue3103Comp>
+    <Vue3103Comp v-bind:localmsg="localmsg2"></Vue3103Comp>
+  </div>
+</template>
+
+<script>
+import Vue3103Comp from '../components/vue3103/Vue3103Comp.vue';
+export default {
+  props: [],
+  data() {
+    return {
+      localmsg1: '부모 컴포넌트에서 자식 컴포넌트로 넘겨진 메세지1',
+      localmsg2: '부모 컴포넌트에서 자식 컴포넌트로 넘겨진 메세지2',
+    };
+  },
+  components: {
+    Vue3103Comp,
+  },
+};
+</script>
