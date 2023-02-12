a732861b981511d7863208da4bf3288cca488190
sookjin01 <aisz2004@naver.com>
2023-02-12 15:00:32 +0900

    리액트 컴포넌트만들기 실습

--- React/rt03.Webpack/webpackprj01/src/App.js
+++ React/rt03.Webpack/webpackprj01/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App({ ...props }) {
   return (
     <div>
-      <div w3-include-header="header.html">
-        <header data-role="header">
-          <h1>Header.html</h1>
-        </header>
-      </div>
+      <Header></Header>
       <section id="page1" data-role="page">
         <div class="content" data-role="content">
           컨텐츠
         </div>
       </section>
-      <div w3-include-footer="footer.html">
-        <footer data-role="footer">
-          <h1>Footer.html</h1>
-        </footer>
-      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ React/rt03.Webpack/webpackprj01/src/Footer.js
@@ -0,0 +1,11 @@
+function Footer({ ...props }) {
+  return (
+    <div w3-include-footer="footer.html">
+      <footer data-role="footer">
+        <h1>Footer Component</h1>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
