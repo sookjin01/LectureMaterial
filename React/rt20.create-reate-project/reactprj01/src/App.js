27bb3ab6cb3d6619c645c3cf72adfd3853b3f886
sookjin01 <aisz2004@naver.com>
2023-02-26 09:41:51 +0900

    커밋

---
+++ React/rt20.create-reate-project/reactprj01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CompStyle from './containers/styled/CompStyle';
+
+function App() {
+  return (
+    <div>
+      <CompStyle></CompStyle>
+    </div>
+  );
+}
+
+export default App;
