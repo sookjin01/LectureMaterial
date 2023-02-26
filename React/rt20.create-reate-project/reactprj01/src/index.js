1a2b4daf9c12d52ef008f856d9bbbaa6998dc647
sookjin01 <aisz2004@naver.com>
2023-02-26 14:01:27 +0900

    라우터 기능 추가

--- React/rt20.create-reate-project/reactprj01/src/index.js
+++ React/rt20.create-reate-project/reactprj01/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+);
+*/
+
+// 라우터 기능 추가 20230226
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
